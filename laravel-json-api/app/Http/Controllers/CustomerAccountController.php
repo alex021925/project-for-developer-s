5c8c53447e841a514b5f5d27a90f1f7bfdbc2ceb
alejandro <alex021925@gmail.com>
2023-11-21 00:29:34 -0500

    Added Customers account Login

---
+++ laravel-json-api/app/Http/Controllers/CustomerAccountController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerAccountRequest;
+use App\Http\Requests\UpdateCustomerAccountRequest;
+use App\Models\CustomerAccount;
+
+class CustomerAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerAccountRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerAccount $customerAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerAccount $customerAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerAccountRequest $request, CustomerAccount $customerAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerAccount $customerAccount)
+    {
+        //
+    }
+}
